4bea17eb2ac8bb8e1b800de8526b78a14f390cca
Parker <park11102001@gmail.com>
2021-06-15 12:46:24 +0700

    connecting the cart provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../providers/cart.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/product.dart';
@@ -18,6 +19,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
     print('product rebuilds');
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -52,7 +54,9 @@ class ProductItem extends StatelessWidget {
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+            },
             color: Theme.of(context).accentColor,
           ),
         ),
