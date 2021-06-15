c3783e83a0a965c174076aea5bfbc620c92c44d6
Parker <park11102001@gmail.com>
2021-06-15 12:24:09 +0700

    added MultiProvider

---
+++ lib/providers/cart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _item;
+
+  Map<String, CartItem> get item {
+    return {..._item};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_item.containsKey(productId)) {
+      _item.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      _item.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
