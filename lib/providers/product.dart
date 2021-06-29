f7a89f85ce2a19ed357a9c325f49a80f435f6a47
Parker <park11102001@gmail.com>
2021-06-29 16:19:33 +0700

    done toggle favorite in firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_app_course1_app4/models/http_exception.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,32 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
     notifyListeners();
   }
+
+  Future<void> toggleFavoriteStatus() async {
+    final url = Uri.https(
+      'flutter-app1-eadee-default-rtdb.asia-southeast1.firebasedatabase.app',
+      '/products/$id.json',
+    );
+    final oldStatus = isFavorite;
+    isFavorite = !isFavorite;
+    notifyListeners();
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+        // throw HttpException('Can not toggle favorite!');
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
+  }
+
 }
