fa6b19c9c89b3a24a01b69abe8b2aafacfdb8cb2
Parker <park11102001@gmail.com>
2021-07-01 21:54:10 +0700

    added authentication screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import './screens/edit_product_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
-
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
